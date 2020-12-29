1fcdcf31390b78b9f656608098f148ddbe5896cf
TheSlowHipster <garnetdroppo@gmail.com>
2020-12-29 14:16:53 -0800

    Working on adding keys

--- bot.py
+++ bot.py
@@ -1,23 +1,84 @@
 import discord
+from discord.ext import commands
 import json
+import logging
+import re
+
+gog      = '^[a-z,A-Z,0-9]{5}-[a-z,A-Z,0-9]{5}-[a-z,A-Z,0-9]{5}-[a-z,A-Z,0-9]{5}$'
+steamOne = '^[a-z,A-Z,0-9]{5}-[a-z,A-Z,0-9]{5}-[a-z,A-Z,0-9]{5}$'
+steamTwo = '^[a-z,A-Z,0-9]{5}-[a-z,A-Z,0-9]{5}-[a-z,A-Z,0-9]{5}-[a-z,A-Z,0-9]{5}-[a-z,A-Z,0-9]{5}$'
+ps3      = '^[a-z,A-Z,0-9]{4}-[a-z,A-Z,0-9]{4}-[a-z,A-Z,0-9]{4}$'
+uplayOne = '^[a-z,A-Z,0-9]{4}-[a-z,A-Z,0-9]{4}-[a-z,A-Z,0-9]{4}-[a-z,A-Z,0-9]{4}$'
+uplayTwo = '^[a-z,A-Z,0-9]{3}-[a-z,A-Z,0-9]{4}-[a-z,A-Z,0-9]{4}-[a-z,A-Z,0-9]{4}-[a-z,A-Z,0-9]{4}$'
+origin   = '^[a-z,A-Z,0-9]{4}-[a-z,A-Z,0-9]{4}-[a-z,A-Z,0-9]{4}-[a-z,A-Z,0-9]{4}-[a-z,A-Z,0-9]{4}$'
+url      = '^http'
+
+description = '''A bot that allows for the addition, retrieval, and storage of game keys
+in a json database for members to share with eachother.'''
+
+logging.basicConfig(level=logging.INFO,filename="discord.log")
 
 with open("conf.json") as f:
 	config = json.load(f)
 
 with open(config["DbFile"]) as f:
-	keys = json.load(f)
+	games = json.load(f)
 
-client = discord.Client()
+intents = discord.Intents.default()
 
-@client.event
-async def on_ready():
-	print(f"Logged in as {client.user}")
+bot = commands.Bot(command_prefix='!', description=description, intents=intents)
 
-@client.event
-async def on_message(message):
-	if message.author == client.user:
-		return
-	if message.content.startsWith('$hello'):
-		await message.channel.send('Hello!')
+@bot.command()
+async def listkeys(ctx):
+	count = 0
+	gms = list(games.keys())
+	while count < len(games.keys()):
+		embed = discord.Embed(
+			title = "Current Key List",
+			description = f"Games {count} to {count+20}"
+		)
+		for i in range(20):
+			if count >= len(gms):
+				break
+			game = gms[count]
+			embed.add_field(name=f"{games[game][0]['GameName']}",value=f'Platform : {games[game][0]["ServiceType"]}\nKeys Available : {len(games[game])}',inline=False)
+			count += 1
+			
+			
+		await ctx.author.send(embed=embed)
 
-client.run(config["Token"])
+@bot.command()
+async def addKey(ctx, *args):
+	serial = args[len(args)-1]
+	auth = ctx.author.name
+	name = ""
+	game = ""
+	for word in args[:len(args)-1]:
+		name += word + " "
+		game += word
+	if re.match(gog,serial) != None:
+		serv = "GoG"
+	elif re.match(steamOne,serial) != None or re.search(steamTwo,serial) != None:
+		serv = "Steam"
+	elif re.match(ps3,serial) != None:
+		serv = "PS3"
+	elif re.match(uplayOne,serial) != None or re.search(uplayTwo,serial) != None:
+		serv = "Uplay"
+	elif re.match(origin,serial) != None:
+		serv = "Origin"
+	elif re.match(url,serial) != None:
+		serv = "Web"
+	else:
+		serv = "Unknown"
+	tmp = {
+			"Author"      : auth,
+			"GameName"    : name,
+			"Serial"      : serial,
+			"ServiceType" : serv
+		}
+	if game not in games.keys():
+		games[game] = tmp
+	else:
+		games[game].append(tmp)
+
+bot.run(config["Token"])
